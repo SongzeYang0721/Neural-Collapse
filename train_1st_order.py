761fc56abfa17990168140cd27763414e9e279f7
Songze_Yang <yangsongze2008@gmail.com>
2024-04-14 22:14:01 +1000

    add print line

--- train_1st_order.py
+++ train_1st_order.py
@@ -46,6 +46,8 @@ def trainer(args, model, trainloader, epoch_id, criterion, optimizer, scheduler,
             if args.loss == 'CrossEntropy':
                 loss = criterion(outputs[0], targets)
             elif args.loss == 'MSE':
+                print("outputs[0]", outputs[0].shape)
+                print("nn.functional.one_hot(targets).type(torch.FloatTensor)", nn.functional.one_hot(targets).type(torch.FloatTensor).shape)
                 loss = criterion(outputs[0], nn.functional.one_hot(targets).type(torch.FloatTensor).to(args.device))
 
         optimizer.zero_grad()
